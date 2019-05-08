96c69fc95a2a58c19917999430be4c396b11ea0c
Tanner Hess Webber <tb@hesswebber.com>
2019-05-08 21:52:01 +0530

    solve webpack-dev-server initial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const App = () => <h1>Hello, World!</h1>;
+// const App = () => React.createElement('h1', { children: 'Hello, World!' });
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(React.createElement(App), document.getElementById('root'));
